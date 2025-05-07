da8b78b1f81ceaf221a08066e15d92bbf8949373
Michael Jung <96927527+lolMichaelJung1@users.noreply.github.com>
2025-05-06 20:42:08 -0700

    Update min_heap.py

--- min_heap.py
+++ min_heap.py
@@ -1,1 +1,17 @@
+import heapq
 
+class MinHeap:
+    def __init__(self):
+        self.heap = []
+
+    def insert(self, item):
+        # TODO: Use heapq to insert item into the heap
+        pass
+
+    def remove_min(self):
+        # TODO: Use heapq to remove and return the smallest item
+        pass
+
+    def peek_min(self):
+        # TODO: Return the smallest item without removing it
+        pass
