d3e14f0bc4d802d9a49712e53bf87cef216cae66
CodingforMegan <79181396+CodingforMegan@users.noreply.github.com>
2025-05-07 17:54:52 -0700

    add debug print statements for each method and track the develop of heap tree in demo

--- min_heap.py
+++ min_heap.py
@@ -1,3 +1,11 @@
+# Created by: Michael Jung (ID:10680322), Timothy Sanders (ID: 01002147), Megan Ng (ID: 00756276)
+
+# Date: 4/21/25
+
+# Course: Spr25_CS_034 CRN 39575
+
+#--------------------------------------------------------------------------------------------
+
 import heapq
 import math
 
